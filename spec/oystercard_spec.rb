1e5884fc3709c34597566b7c8de7377c05b54cac
sumeet978 <searchsumeet@gmail.com>
2019-07-31 15:44:33 +0100

    Second commit for task 10

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,12 +14,14 @@ describe Oystercard do
     expect{ subject.top_up 1 }.to raise_error 'Exceeded Maximum Balance'
   end
 
-  it { is_expected.to respond_to(:deduct).with(1).argument }
-   
-  it "deducts and amount from the balance" do
-    subject.top_up(20)
-    expect{ subject.deduct(3) }.to change{ subject.balance }.by (-3)
-  end
+  # describe "#deduct" do
+  # it { is_expected.to respond_to(:deduct).with(1).argument }
+  # end
+
+  # it "deducts and amount from the balance" do
+  #   subject.top_up(20)
+  #   expect{ subject.deduct(3) }.to change{ subject.balance }.by (-3)
+  # end
   
   it "is initially not in a journey" do
     expect(subject).not_to be_in_journey
@@ -44,9 +46,9 @@ describe Oystercard do
     expect{ subject.touch_in }.to raise_error "Insufficient balance to touch in"
   end
 
- it "checks if charge is made on touch out" do
-  subject.top_up(20)
-  expect{ subject.deduct(1) }.to change{ subject.balance }.by (-Oystercard::MINIMUM_CHARGE)
- end
+#  it "checks if charge is made on touch out" do
+#   subject.top_up(20)
+#   expect{ subject.deduct(5) }.to change{ subject.balance }.by (-5)
+#  end
 end
 
